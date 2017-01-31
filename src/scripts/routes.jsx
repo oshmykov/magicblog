57887177ac9718449638666723bd4b3c77ba6330
Oleg <shmykov.oleg@gmail.com>
2017-01-30 23:03:04 -0500

    [#6] 6 Post Manage Form is created with validation. Connected to the store

--- src/scripts/routes.jsx
+++ src/scripts/routes.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import MagicHome from '~/components/containers/MagicHome';
 import PostDetails from '~/components/containers/PostDetails';
+import ManagePost from '~/components/containers/ManagePost';
 
 const routes = (
 	<Route>
 		<Route path="/(:page)" component={MagicHome} />
 		<Route path="/:year/:month/:day/:postId" component={PostDetails} />
+		<Route path="/post/new" component={ManagePost} />
+		<Route path="/post/edit/:postId" component={ManagePost} />
 	</Route>
 );
 
