55e76b48e539f83a0295ac870689dfdbbb7521cc
Oleg <shmykov.oleg@gmail.com>
2017-02-04 00:56:04 -0500

    [#13] MagicBlog-13. Navigation Bar and Layout

--- src/scripts/routes.jsx
+++ src/scripts/routes.jsx
@@ -3,20 +3,25 @@ import { Route, IndexRoute, Redirect, IndexRedirect } from 'react-router';
 
 import store from '~/store'
 
+import App from '~/components/containers/App';
 import MagicHome from '~/components/containers/MagicHome';
 import PostDetails from '~/components/containers/PostDetails';
 import ManagePost from '~/components/containers/ManagePost';
 
 const routes = (
 	<Route>
-		<Route path="/:year/:month/:day/:postId" component={PostDetails} />
-		<Route path="post" onEnter={requireAuth}>
-			<Route path="new" component={ManagePost} />
-			<Route path="edit/:postId" component={ManagePost} />
-			<Redirect from="*" to="new" />
-			<IndexRedirect to="new" />
+		<Route path="/" component={App}>
+			<Route path="/:year/:month/:day/:postId" component={PostDetails} />
+			<Route path="post" onEnter={requireAuth}>
+				<Route path="new" component={ManagePost} />
+				<Route path="edit/:postId" component={ManagePost} />
+				<Redirect from="*" to="new" />
+				<IndexRedirect to="new" />
+			</Route>
+			<Route path=":page" component={MagicHome} />			
+			<IndexRoute component={MagicHome} />
 		</Route>
-		<Route path="/(:page)" component={MagicHome} />
+
 	</Route>
 );
 
